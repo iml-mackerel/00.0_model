4a0cb4103d6b4ecb35f1459f3888ba890fbacdd5
clehoux <caroline.lehoux@dfo-mpo.gc.ca>
2026-05-08 15:28:08 -0400

    patch

--- R/multi.forecast.R
+++ R/multi.forecast.R
@@ -7,6 +7,7 @@
 ##' @import parallel
 ##' @rdname multi.forecast
 ##' @export
+source("C:/LEHOUX/Maquereau/iml-mackerel/00.0_model/Rscripts/2024/surplus/forecast_patch.R")
 multi.forecast <- function(x,dir,parallel=FALSE,ncores=NULL){
     if(parallel){
         library(parallel)
@@ -15,13 +16,13 @@ multi.forecast <- function(x,dir,parallel=FALSE,ncores=NULL){
         clusterEvalQ(cl, {library(CCAM)}) #load the package to each node
         clusterExport(cl, c('dir','x','ctUSA'), envir=environment())
         empty <- parLapply(cl, 1:length(x), function(y){ 
-            run <- do.call(forecast, x[[y]])
+            run <- do.call(forecast.patch, x[[y]])
             save(run,file=paste0(dir,names(x)[y],'.Rdata'))
         })
         stopCluster(cl) 
     }else{
         empty <- lapply(1:length(x),function(y){
-            run <- do.call(forecast, x[[y]])
+            run <- do.call(forecast.patch, x[[y]])
             save(run,file=paste0(dir,names(x)[y],'.Rdata'))
         }) 
     }
